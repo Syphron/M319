ef87e193cd7535403119be65816a1b57f6fa345e
Syphron <julian.odermatt@gmx.ch>
2024-01-05 11:24:34 +0100

    added more products

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,32 +1,43 @@
-using projekt;
+// Julian Odermatt, Ramon Wyss, S-INF-22dL
+
+using projekt;  // verwendet den namespace projekt, woring die produkt- und Automatenklassen definiert sind
+
 
 namespace TestProject1
 {
-    public class Tests
-    {
-        public Automat _automat;
+    public class Tests { 
+    
+        public Automat _automat;  // deklariert eine variable vom Typ 'Automat'.
 
+        // Vorbereitung für den Test
         [SetUp]
         public void Setup()
         {
             _automat = new Automat();
-            _automat.AddProdukt(new Snack(1, "Chips", 2.50m));
-            _automat.AddProdukt(new Getränk(2, "Cola", 1.50m));
+
+            // fügt produkte hinzu
+            _automat.AddProdukt(new Snack(1, "Chips", 2.50m, "Zweifel"));
+            _automat.AddProdukt(new Snack(2, "Waffel", 3.30m, "Werner"));
+            _automat.AddProdukt(new Getraenk(3, "Cola", 1.50m, "Coca Cola"));
+            _automat.AddProdukt(new Getraenk(4, "Redbull", 2.20m, "Redbull"));
+            _automat.AddProdukt(new Getraenk(5, "Fanta", 1.50m, "Coca Cola"));
         }
 
         [Test]
-        public void Test1()
+        // testet ob ein produkt gültig ist und vorhanden ist
+        public void GueltigesProdukt()
         {
-            var produkt = _automat.WähleProdukt(1);
+            var produkt = _automat.WaehleProdukt(1);
 
             Assert.That(produkt, Is.Not.Null);
             Assert.That(produkt.Name, Is.EqualTo("Chips"));
         }
 
         [Test]
-        public void GültigeNummern()
+        // Testet ob die Abfrage von einem nicht vorhandenen Produkt null zurückgibt
+        public void GueltigeNummern()
         {
-            var produkt = _automat.WähleProdukt(99);
+            var produkt = _automat.WaehleProdukt(99);
 
             Assert.That(produkt, Is.Null);
         }
--- m316/Program.cs
+++ m316/Program.cs
@@ -1,5 +1,4 @@
-﻿
-
+﻿// Julian Odermatt, Ramon Wyss, S_INF_22dL
 
 namespace projekt
 {
@@ -7,49 +6,59 @@ namespace projekt
     using System;
     using System.Collections.Generic;
 
+    // interface definieren
     public interface IProdukt
     {
         int Nummer { get; }
         string Name { get; }
         decimal Preis { get; }
+        string Hersteller { get; }
     }
 
+    // abstrake Klasse für die Produkte
     public abstract class Produkt : IProdukt
     {
         public int Nummer { get; protected set; }
         public string Name { get; protected set; }
         public decimal Preis { get; protected set; }
+        public string Hersteller { get; protected set; }
 
-        protected Produkt(int nummer, string name, decimal preis)
+        protected Produkt(int nummer, string name, decimal preis, string hersteller)
         {
             Nummer = nummer;
             Name = name;
             Preis = preis;
+            Hersteller = hersteller;
         }
     }
 
+     // implementierung von dem Produkt für die Snacks
     public class Snack : Produkt
     {
-        public Snack(int nummer, string name, decimal preis) : base(nummer, name, preis) { }
+        public Snack(int nummer, string name, decimal preis, string hersteller) : base(nummer, name, preis, hersteller) { }
     }
 
-    public class Getränk : Produkt
+    // implementierung von dem Produkt für die Snacks
+    public class Getraenk : Produkt
     {
-        public Getränk(int nummer, string name, decimal preis) : base(nummer, name, preis) { }
+        public Getraenk(int nummer, string name, decimal preis, string hersteller) : base(nummer, name, preis, hersteller) { }
     }
 
+    // klasse für den Autoaten, der die Produkte speichert und verwaltet
     public class Automat
     {
         List<IProdukt> produktListe = new List<IProdukt>();
         IProdukt[] produkteArray;
 
+        // fügt ein produkt zum automaten hinzu
         public void AddProdukt(IProdukt produkt)
         {
             produktListe.Add(produkt);
             produkteArray = produktListe.ToArray();
         }
 
-        public IProdukt WähleProdukt(int nummer)
+        // wählt ein Produkt anhand seiner Nummer aus
+        public IProdukt WaehleProdukt(int nummer)
         {
 
             foreach (var produkt in produkteArray)
@@ -62,6 +71,7 @@ namespace projekt
             return null;
         }
 
+        // Berechnung des Rückgeldes
         public decimal Zahle(decimal betrag, decimal preis)
         {
             return betrag - preis;
@@ -74,13 +84,21 @@ namespace projekt
         static void Main()
         {
             Automat automat = new Automat();
-            automat.AddProdukt(new Snack(1, "Chips", 2.50m));
-            automat.AddProdukt(new Getränk(2, "Cola", 1.50m));
 
-            Console.WriteLine("Wählen Sie ein Produkt durch Eingabe der Nummer:");
+            // initialisiert den Automaten mit verschiedenen Produkten
+            automat.AddProdukt(new Snack(1, "Chips", 2.50m, "Zweifel"));
+            automat.AddProdukt(new Snack(2, "Waffel", 3.30m, "Werner"));
+            automat.AddProdukt(new Getraenk(3, "Cola", 1.50m, "Coca Cola"));
+            automat.AddProdukt(new Getraenk(4, "Redbull", 2.20m, "Redbull"));
+            automat.AddProdukt(new Getraenk(5, "Fanta", 1.50m, "Coca Cola"));
+
+            
+            // Eingabebeaufforderung um ein Produkt auszuwählen
+            Console.WriteLine("Waehlen Sie ein Produkt durch Eingabe der Nummer:");
             int nummer = Convert.ToInt32(Console.ReadLine());
-            IProdukt produkt = automat.WähleProdukt(nummer);
+            IProdukt produkt = automat.WaehleProdukt(nummer);
 
+            // überprüfung ob ein Produkt vorhanden ist oder nicht
             if (produkt == null)
             {
                 Console.WriteLine("Dieses Produkt gibt es nicht.");
@@ -88,14 +106,15 @@ namespace projekt
             else
             {
                 decimal bezahlt = 0m;
+                // schleife welche solange ausgeführt wird bis der Betrag bezahlt wurde
                 while (bezahlt < produkt.Preis)
                 {
                     Console.WriteLine($"Bitte zahlen Sie {produkt.Preis - bezahlt:C}.");
                     bezahlt += Convert.ToDecimal(Console.ReadLine());
                 }
-
+                // berechnet das Rückgeld und gibt es aus
                 decimal rückgeld = automat.Zahle(bezahlt, produkt.Preis);
-                Console.WriteLine($"Produkt ausgeworfen. Rückgeld: {rückgeld:C}");
+                Console.WriteLine($"Produkt ausgeworfen. Rueckgeld: {rückgeld:C}");
             }
         }
     }
